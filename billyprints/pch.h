6c776033c50cba97a99a6a5b37098a97321dbbad
Billy <billydevbusiness@gmail.com>
2022-07-27 03:06:54 +0800

    Initial commit (project setup, dependencies)

---
+++ billyprints/pch.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "imgui.h"
+#include "imgui_internal.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+#include "ImNodes.h"
+#include "ImNodesEz.h"
+
+#include <vector>
+#include <map>
+#include <string>
+#include <stdio.h>
+#include <GLFW/glfw3.h>
